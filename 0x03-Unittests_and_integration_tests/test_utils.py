20c8829c300ceef08083ba90ca6b461301c159ea
Terry <terrybmochire@gmail.com>
2023-01-31 00:13:09 +0300

    Task 0.

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Test suite for utils.py
+"""
+
+import unittest
+from typing import Mapping, Sequence, Any
+
+from parameterized import parameterized
+
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(
+        self, nested_map: Mapping, path: Sequence, expected: Any
+    ):
+        """Test access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
